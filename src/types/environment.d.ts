c4d28b3ae6f02693b889bfdacdc327b1abbc0bb8
mahmoud manfy <eng-mahmoud.manfy1722@alexu.edu.eg>
2022-05-24 13:32:12 +0200

    sever app

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -2,10 +2,7 @@ declare global {
   namespace NodeJS {
     interface ProcessEnv {
       MONGO_URI: string;
-      MONGO_URI_LOCAL: string;
-      NODE_ENV: 'development' | 'production' | 'test';
       PORT?: number;
-      MAIN_ADMIN_ID: string;
     }
   }
 }
